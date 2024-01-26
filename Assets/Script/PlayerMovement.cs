2f36db0b78dfb9ec1b4f0a1fd114ebdeda8b176f
Mathys Hymon <mathys.hymon@gmail.com>
2024-01-26 14:13:23 +0100

    [fix] movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private float accel;
 
-    // Update is called once per frame
-    void Update()
+    private float speed;
+
+    private void Update()
     {
-        
+        transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+           if(speed < maxSpeed)
+            {
+                speed += accel * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if(speed > 0)
+            {
+                speed -= accel * Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if (speed > 0)
+            {
+                speed -= accel/5 * Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.D)) 
+        { 
+
+        }
     }
 }
